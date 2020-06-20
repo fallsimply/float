3c327f4dbbee3d7ba9e10313009f3b0f1656f399
fall simply <simply@fallproject.org>
2020-06-20 00:14:57 -0500

    convert identation to tabs in auto generated files

--- src/router/index.js
+++ src/router/index.js
@@ -5,22 +5,22 @@ import Home from '../views/Home.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Edit',
-    component: () => import(/* webpackChunkName: "edit" */ '../views/Edit.vue')
-  },
-  {
-    path: '/home',
-    name: 'Home',
-    component: Home
-  },
+	{
+		path: '/',
+		name: 'Edit',
+		component: () => import(/* webpackChunkName: "edit" */ '../views/Edit.vue')
+	},
+	{
+		path: '/home',
+		name: 'Home',
+		component: Home
+	},
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+	mode: 'history',
+	base: process.env.BASE_URL,
+	routes
 })
 
 export default router
