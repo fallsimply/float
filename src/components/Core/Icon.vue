4825a6dffe9b51ae5f7fc5968b7978d412a5948f
fall simply <simply@fallproject.org>
2020-09-30 05:00:10 +0000

    Add Icons

---
+++ src/components/Core/Icon.vue
@@ -0,0 +1,33 @@
+<template>
+		<svg :viewBox="`0 0 ${caret ? 36 : 24} 24`">
+			<use :xlink:href="`/icons/${pack}.svg#${name}`" width="24" fill="currentColor"/>
+			<use v-if="caret" xlink:href="/icons/caret.svg#icon" width="12" fill="currentColor" x="24" id="caret"/>
+		</svg>
+</template>
+
+<script>
+export default {
+	name: "Icon",
+	props: {
+		caret: {
+			type: Boolean,
+			default: false,
+		},
+		name: {
+			type: String,
+			required: true,
+		},
+		pack: {
+			type: String,
+			default: "general"
+		}
+	}
+}
+</script>
+
+<style lang="stylus">
+svg
+	#caret
+		transform-origin: center;
+		transition: transform .2s;
+</style>
